754b3637b7245998894e76a05b350323c7834278
Rishad <rrrahman@mit.edu>
2015-05-03 19:35:04 -0400

    added data type definition for expression

--- src/expresso/Expression.java
+++ src/expresso/Expression.java
@@ -1,13 +1,15 @@
 package expresso;
 
 /**
- * TODO Specification
+ * 
  */
 public interface Expression {
     
     // Datatype definition
-    //   TODO
-    
+    //  Expression = Empty + NumberExp + OperationExp + ParenExp(Expression: e)
+    //  NumberExp = IntegerExp(int n) + DecimalExp(double d)
+    //  OperationExp = AddExp(Expression e1, Expression e2) + MultExp(Expression e1, Expressione2)
+
     /**
      * Parse an expression.
      * @param input expression to parse
