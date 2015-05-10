f5e3f73d64e4372797537ec1815532f768e60937
Rishad <rrrahman@mit.edu>
2015-05-09 22:31:35 -0400

    MonomialComparator and SimpleExpressionIterator skeleton

---
+++ src/expresso/MonomialComparator.java
@@ -0,0 +1,13 @@
+package expresso;
+
+import java.util.Comparator;
+
+public class MonomialComparator implements Comparator<Monomial>{
+
+    @Override
+    public int compare(Monomial arg0, Monomial arg1) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
