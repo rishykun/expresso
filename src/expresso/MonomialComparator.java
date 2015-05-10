dac0df10cb402fe210ef3ac713f0aaa7227adc38
Gabriel Esayas <gesayas@mit.edu>
2015-05-10 00:24:14 -0400

    Implemented the parse method for Expression and a comparator for Monomials (adding a method for Monomials to return a copy of their TreeMaps to do so)

--- src/expresso/MonomialComparator.java
+++ src/expresso/MonomialComparator.java
@@ -1,13 +1,27 @@
 package expresso;
 
 import java.util.Comparator;
+import java.util.TreeMap;
 
 public class MonomialComparator implements Comparator<Monomial>{
 
     @Override
-    public int compare(Monomial arg0, Monomial arg1) {
-        // TODO Auto-generated method stub
-        return 0;
+    public int compare(Monomial first, Monomial second) {
+        TreeMap<Variable, Integer> firstMap = first.getMap();
+        TreeMap<Variable, Integer> secondMap = second.getMap();
+        int largestOfFirst = -1;
+        int largestOfSecond = -1;
+        for (int exp : firstMap.values()){
+            if (exp > largestOfFirst){
+                largestOfFirst = exp;
+            }
+        }
+        for (int exp : secondMap.values()){
+            if (exp > largestOfSecond){
+                largestOfSecond = exp;
+            }
+        }
+        return (largestOfFirst < largestOfSecond ? -1 : (largestOfFirst == largestOfSecond ? 0 : 1));
     }
 
 }
