e33c74424ade5cf6fa838c854831930ef1ff3fd4
Vincent Anioke <aniokevu@mit.edu>
2015-05-10 02:48:21 -0400

    Deleted Iterator.java and implemented an iterator method for SimpleExpressions instead, added methods to Monomial and SimpleExpression to make iteration easier, implemented the unimplemented methods of Expressions.java

--- src/expresso/SimpleExpression.java
+++ src/expresso/SimpleExpression.java
@@ -1,11 +1,14 @@
 package expresso;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class SimpleExpression implements Expression {
 
     private Monomial leading;
     private SimpleExpression remaining;
+    private boolean isMonomial = false;
     
     public SimpleExpression(Monomial m, SimpleExpression e){
         leading = m;
@@ -37,12 +40,35 @@ public class SimpleExpression implements Expression {
     public SimpleExpression simplify() {
         return this;
     }
+    
+    public boolean checkMonomial() {
+        return isMonomial;
+        
+        
+    }
 
     public Iterator<Monomial> iterator(){
-        return new SimpleExpressionIterator(this);
+        List<Monomial> listOfTerms = new ArrayList<Monomial>();
+        SimpleExpression currentExpression = this;
+        
+        while (!currentExpression.checkMonomial()){
+            Monomial currentLeading = currentExpression.leading();
+            listOfTerms.add(0, currentLeading);
+            currentExpression = currentExpression.truncate();
+        }
+        Monomial lastMonomial = currentExpression.leading();
+        listOfTerms.add(0, lastMonomial);
+        return listOfTerms.iterator();
+        
+    }
+    
+    public Monomial leading(){
+        return leading;
     }
     
     public SimpleExpression truncate(){
         return remaining;
     }
+
+    
 }
