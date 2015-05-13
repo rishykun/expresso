f6c9002fde3b4dc89cd35f51f0b9447c10d21088
Vincent Anioke <aniokevu@mit.edu>
2015-05-13 12:49:33 -0400

    fixed a bug hopefully

--- src/expresso/Expressions.java
+++ src/expresso/Expressions.java
@@ -13,6 +13,9 @@ public class Expressions {
      * @throws IllegalArgumentException if the expression or variable is invalid
      */
     public static String differentiate(String expression, String variable) throws IllegalArgumentException{
+        if (!variable.matches("[A-Za-z]+")){
+            throw new IllegalArgumentException();
+        }
         Expression parsedInput = Expression.parse(expression);
         Expression diffInput = parsedInput.differentiate(new Variable(variable));
         return diffInput.toString();
